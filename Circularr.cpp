fecfb738c2d25e78bca81a70594eb6571a0aacf2
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-29 15:35:27 +0700

    menambahkan is else di else nya

--- Circularr.cpp
+++ Circularr.cpp
@@ -36,5 +36,12 @@ public:
             REAR = 0;
         }
         else
+        {
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
         }
+        queue_array[REAR] = num;
+    }
 }
