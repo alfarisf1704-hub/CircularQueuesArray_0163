8e67f5dd33f5ca14731b101a3c1b3bee848bb730
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-29 15:51:50 +0700

    menambahkan while

--- Circularr.cpp
+++ Circularr.cpp
@@ -84,5 +84,14 @@ public:
         }
 
         cout << "\nElements in the queue are ...\n";
+
+        // jika FRONT <= REAR, iterasi FRONT hingga REAR
+        if (FRONT_Position <= REAR_Position)
+        {
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout <<
+            }
+        }
     }
 }
