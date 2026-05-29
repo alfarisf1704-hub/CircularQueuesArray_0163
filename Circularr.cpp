94f2cbc9902999f98858cd4f1049bcee11d8e602
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-29 15:31:40 +0700

    menambahkan cout, dan keterang (komen)

--- Circularr.cpp
+++ Circularr.cpp
@@ -22,8 +22,11 @@ public:
         cin >> num;
         cout << endl;
 
+        // apakah antrian penuh
         if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
         {
+            cout << "\nQueue overflow\n";
+            return;
         }
     }
 }
