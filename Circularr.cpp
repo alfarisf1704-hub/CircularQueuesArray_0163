6dced4fc04e68e435db393de878b612f6e41b153
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-05-29 15:43:20 +0700

    menambahkan fungsi if else di else

--- Circularr.cpp
+++ Circularr.cpp
@@ -60,6 +60,10 @@ public:
             REAR = -1;
         }
         else
-        // jika element yg dihapus berada di posisi terakhir array, maka kembali ke awal array
+            // jika element yg dihapus berada di posisi terakhir array, maka kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
     }
 }
